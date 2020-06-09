74f5b82545eb84393b2df8cf33577076b24406dc
gherardovarando <gherardo.varando@gmail.com>
2020-06-09 07:07:02 +0200

    Improve code style
    * replace double 0/1 as 0.0E+0/1.0E+0
    * reduce the amount of for cycles to minimum
    * add IMPLICIT NONE statement

--- nodag.f
+++ nodag.f
@@ -1,12 +1,13 @@
       SUBROUTINE NODAG(N, SIGMA, A, LAMBD, EPS, ALPHA, MAXITR)
-c     NODAG routine (version 0.0.1)  
+c     NODAG routine (version 0.0.2)  
 c     gherardo varando (2020) <gherardo.varando[at]gmail.com>
 c
 c     Find a sparse parametrization of the inverse covariance
 c     matrix as  A A**t using a proximal gradient algorithm 
 c     for the problem
-c               minimize   -LOGLIK(AA**t | SIGMA) + LAMBD * ||A||_1 
-c
+c             minimize   -LOGLIK(AA**t | SIGMA) + LAMBD * ||A||_1 
+c             A invertible
+      IMPLICIT NONE
 c     integer variables 
       INTEGER N,MAXITR
 c     double precision variables
@@ -52,18 +53,18 @@ c     internal variables
       PARAMETER ( ONE = 1.0E+0 )
       PARAMETER ( ZERO = 0.0E+0 )
       ITR = 0
-      F = 0
+      F = 0.0E+0
 c     initialize penalty for A = identity 
       G = LAMBD * N
 c     initialize matrices and F = trace(SIGMA)  
       DO 20 J = 1,N 
          DO 10 I = 1,N
-            A(I,J) = 0
-            TMP(I,J) = 0
+            A(I,J) = 0.0E+0
+            TMP(I,J) = 0.0E+0
             D(I,J) = SIGMA(I,J)
   10     CONTINUE          
-         A(J,J) = 1
-         TMP(J,J) = 1
+         A(J,J) = 1.0E+0
+         TMP(J,J) = 1.0E+0
          IPIV(J) = J
          F = F + SIGMA(J,J)
   20  CONTINUE 
@@ -75,36 +76,28 @@ c     main loop here, increase iteration counter
 c     compute TMP = TMP**-1
       CALL DGETRI(N, TMP, N, IPIV, WORK, 2*N, INFO)
 c     compute GRD = 2*(D - TMP**t)
-      DO 35 J=1,N
-         DO 32 I=1,N
+c     and copy A before starting line search 
+      DO 40 J=1,N
+         DO 30 I=1,N
             GRD(I,J) = 2*(D(I,J) - TMP(J,I))
-  32     CONTINUE   
-  35  CONTINUE
-c     copy old A before starting line search 
-      DO 90 J = 1,N 
-         DO 80 I = 1,N
-             AOLD(I,J) = A(I,J)
-  80     CONTINUE          
-  90  CONTINUE 
+            AOLD(I,J) = A(I,J)
+  30     CONTINUE   
+  40  CONTINUE
       STEP = 1
 c     line search loop
   600 CONTINUE     
 c     gradient step
-      DO 110 J = 1,N 
-         DO 100 I = 1,N
+c     and soft thresholding
+      DO 110 J =1,N
+         DO 100 I=1,N
             A(I,J) = AOLD(I,J) - STEP * GRD(I,J) 
-  100    CONTINUE
-  110 CONTINUE
-c     soft thresholding
-      DO 130 J =1,N
-         DO 120 I=1,N
             A(I,J) = SIGN(ONE,A(I,J))*(ABS(A(I,J))-STEP*LAMBD) 
             IF (ABS(A(I,J)) .LE. STEP*LAMBD) THEN
-               A(I,J) = 0
+               A(I,J) = 0.0E+0
             ENDIF
             TMP(I,J) = A(I,J)
- 120     CONTINUE
- 130  CONTINUE
+ 100     CONTINUE
+ 110  CONTINUE
 c     compute TMP = PLU 
       CALL DGETRF(N, N, TMP, N, IPIV,INFO)  
       IF (INFO .GT. 0) THEN
@@ -114,9 +107,9 @@ c     compute TMP = PLU
 c     compute D = SIGMA * A
       CALL DSYMM("L","U", N, N, ONE, SIGMA, N, A, N, ZERO, D, N)  
 c     compute FNW, GNW in new A
-      FNW = 0
-      GNW = 0
-      DIFF = 0
+      FNW = 0.0E+0
+      GNW = 0.0E+0
+      DIFF = 0.0E+0
 c     compute F = -2*LOG(DET(A)) + trace(A**t SIGMA A) 
 c             and G = ||A||_1
       DO 150 J=1,N
